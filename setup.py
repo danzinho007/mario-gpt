a00f5bc7894986ccc142bb994bcde23a459c6e48
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-19 13:17:39 -0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+# Importando :
+# io = Ferramenta para Trabalhar com E/S ( Entrada / Saída)
+# os : Usar recursos do S.O como ler ou gravar arquivos
+# re : Módulo de Expressões Regulares
+# find_packages da Biblioteca setuptools : Funções Específicas do path sem precisar digitar 'os.path' toda vez
+# setup da Biblioteca setuptools : Definir e Configurar pacotes Python ( Empacotar )
+
 import io
 import os
 import re
@@ -6,11 +13,37 @@ from os import path
 from setuptools import find_packages
 from setuptools import setup
 
+# Define a variável "this_directory" para ser o diretório absoluto do arquivo de configuração setup.py
+# path.dirname(__file__) = Função da Biblioteca padrão "os.path" do Python que retorna o nome do diretório do arquivo Python atual
+# "__file__" = É um argumento / variável pré-definidade no Python que contém o caminho absoluto do arquivo Python em que está sendo executado
+# Ao chamar path.dirname() com __file__ como argumento, a função retorna o nome do diretório em que o arquivo Python atual está localizado.
+# Por exemplo, se o arquivo Python estiver localizado em /home/user/projeto/meuarquivo.py, path.dirname(__file__) retornará /home/user/projeto, que é o nome do diretório que contém o arquivo Python atual.
+# A função path.abspath() é então usada para retornar o caminho absoluto do diretório, que pode ser usado para acessar arquivos em diretórios relacionados ao arquivo Python atual.
+
+# Função "open()" é usada para abrir o arquivo README.md no mesmo diretóio, combinando o diretório atual com o nome do arquivo usando a função "join()" da biblioteca "os.path"
+
+# O conteúdo do arquivo é lido com o método "read()" da variável f e atribuído a variável "long_description" que será usada como a descrição longa do pacote no PyPI ( Python Package Index ) quando o pacote for distribuído
+# PyPI é a sigla para "Python Package Index". Trata-se de um repositório público de pacotes de software Python que podem ser instalados usando o gerenciador de pacotes pip.
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# setup() = Função do "setuptools" que permite definir as informações sobre o pacote e suas dependências.
+# name: o nome do pacote.
+# version: a versão atual do pacote.
+# url: o URL do site oficial do pacote.
+# license: a licença sob a qual o pacote é distribuído.
+# author: o nome do autor do pacote.
+# author_email: o endereço de e-mail do autor do pacote.
+# description: uma descrição curta do pacote.
+# long_description: uma descrição mais longa do pacote, geralmente em formato markdown.
+# long_description_content_type: o tipo de conteúdo da descrição longa, que geralmente é "text/markdown".
+# packages: uma lista de pacotes incluídos no pacote.
+# install_requires: uma lista de pacotes Python necessários para que o pacote funcione corretamente.
+# classifiers: uma lista de strings que descrevem o estado de desenvolvimento do pacote, sua compatibilidade com diferentes versões do Python e outros metadados.
+
+# Ao definir esses argumentos dentro de setup(), é possível criar um pacote Python que pode ser facilmente instalado e distribuído usando o pip e outros gerenciadores de pacotes.
 
 setup(
     name="mario-gpt",
